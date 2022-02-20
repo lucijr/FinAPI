b53c9a67edefdbbc6f9577f7b367220b46c583f7
lucijr <luciano@mecpock.com>
2022-02-19 21:58:24 -0300

    create: Route to created user

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+const { response } = require("express");
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+//Base Config
 const app = express();
+app.use(express.json());
+//Users
+const customers = [];
+app.post("/accounts", (request, response) => {
+  const { name, cpf } = request.body;
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+  if (customerAlreadyExists) {
+    return response.status(400).send({ error: "Customer already exists!" });
+  }
+  customers.push({
+    name,
+    cpf,
+    id: uuidv4(),
+    statment: [],
+  });
+  return response.status(201).send(customers);
+});
+//Finish Users
 app.listen(3333);
